832438efe952cfe36dc7ab3b078ab360982e402e
Mahdi Ketabdar <mahdi7192@gmail.com>
2016-01-04 18:17:32 +0330

    process list ro dorost kardam

--- public/js/function.js
+++ public/js/function.js
@@ -36,3 +36,18 @@ function selectAllCheckboxes(){
     selectAll = false;
   }
 }
+
+function showProcessList(){
+  $("#process-list").addClass("show");
+  $("#navbar").addClass("blur");
+  $("#container").addClass("blur");
+  $("#wrapper").css("display", "block");
+  $("#addNewProcess").animate({bottom: "20px"}, "slow");
+}
+function hideProcessList(){
+  $("#process-list").removeClass("show");
+  $("#navbar").removeClass("blur");
+  $("#container").removeClass("blur");
+  $("#wrapper").css("display", "none");
+  $("#addNewProcess").animate({bottom: "-100px"}, "fast");
+}
